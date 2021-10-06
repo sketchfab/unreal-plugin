4f5b3f441e6856797edb30b7d7729647f87a0988
Loïc Norgeot <loic.norgeot@sketchfab.com>
2021-10-06 20:46:34 +0200

    Replaces accessor parsing with their refactored versions

--- Source/SketchfabGLTF/Private/StaticMeshImporter.cpp
+++ Source/SketchfabGLTF/Private/StaticMeshImporter.cpp
@@ -199,268 +199,88 @@ UStaticMesh* FGLTFStaticMeshImporter::ImportStaticMesh(FGLTFImportContext& Impor
 		int32 TangentZOffset = RawTriangles.WedgeTangentZ.Num();
 		int32 NumFaces = 0;
 
+		tinygltf::Accessor* accessor = nullptr;
+		unsigned char* rawData;
+
 		// Indices
+		rawData = GetAccessorData(model, prim, accessor, "");
+		if (rawData && accessor)
 		{
-			int accessorIndex = prim.indices;
-			if (accessorIndex >= 0 && accessorIndex < model->accessors.size())
+			NumFaces = accessor->count / 3;
+			ensure(accessor->type == TINYGLTF_TYPE_SCALAR);
+			if (accessor->type == TINYGLTF_TYPE_SCALAR)
 			{
-				const tinygltf::Accessor &accessor = model->accessors[accessorIndex];
-				const tinygltf::BufferView &bufferView = model->bufferViews[accessor.bufferView];
-				const tinygltf::Buffer &buffer = model->buffers[bufferView.buffer];
-				size_t offset = accessor.byteOffset + bufferView.byteOffset;
-
-				if (accessor.count > 0)
+				RawTriangles.WedgeIndices.AddZeroed(accessor->count);
+				switch (accessor->componentType)
 				{
-					NumFaces = accessor.count / 3;
-					RawTriangles.WedgeIndices.AddZeroed(accessor.count);
-					ensure(accessor.type == TINYGLTF_TYPE_SCALAR);
-					if (accessor.type == TINYGLTF_TYPE_SCALAR)
-					{
-						switch (accessor.componentType)
-						{
-						case TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT:
-						{
-							unsigned short *data = (unsigned short*)&buffer.data[offset];
-							for (int i = 0; i < accessor.count; i++)
-							{
-								RawTriangles.WedgeIndices[WedgeOffset + i] = VertexOffset + data[i];
-							}
-						}
-						break;
-						case TINYGLTF_COMPONENT_TYPE_INT:
-						{
-							int32 *data = (int32*)&buffer.data[offset];
-							for (int i = 0; i < accessor.count; i++)
-							{
-								RawTriangles.WedgeIndices[WedgeOffset + i] = VertexOffset + data[i];
-							}
-						}
-						break;
-						case TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT:
-						{
-							uint32 *data = (uint32*)&buffer.data[offset];
-							for (int i = 0; i < accessor.count; i++)
-							{
-								RawTriangles.WedgeIndices[WedgeOffset + i] = VertexOffset + data[i];
-							}
-						}
-						break;
-						default: ensure(false); break;
-						}
-					}
+				case TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT: parseIndicesData<unsigned short>(rawData, accessor->count, RawTriangles, VertexOffset, WedgeOffset); break;
+				case TINYGLTF_COMPONENT_TYPE_INT:            parseIndicesData<int32>(rawData, accessor->count, RawTriangles, VertexOffset, WedgeOffset); break;
+				case TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT:   parseIndicesData<uint32>(rawData, accessor->count, RawTriangles, VertexOffset, WedgeOffset); break;
+				default: ensure(false); break;
 				}
 			}
 		}
 
-		const auto &position = prim.attributes.find("POSITION");
-		if (position != prim.attributes.end())
+		rawData = GetAccessorData(model, prim, accessor, "POSITION");
+		if (rawData && accessor)
 		{
-			int accessorIndex = position->second;
-			if (accessorIndex >= 0 && accessorIndex < model->accessors.size())
+			ensure(accessor->type == TINYGLTF_TYPE_VEC3);
+			if (accessor->type == TINYGLTF_TYPE_VEC3)
 			{
-				const tinygltf::Accessor &accessor = model->accessors[accessorIndex];
-				const tinygltf::BufferView &bufferView = model->bufferViews[accessor.bufferView];
-				const tinygltf::Buffer &buffer = model->buffers[bufferView.buffer];
-				size_t offset = accessor.byteOffset + bufferView.byteOffset;
-
+				RawTriangles.VertexPositions.AddZeroed(accessor->count);
+				switch (accessor->componentType)
 				{
-					RawTriangles.VertexPositions.AddZeroed(accessor.count);
-					ensure(accessor.type == TINYGLTF_TYPE_VEC3);
-					if (accessor.type == TINYGLTF_TYPE_VEC3)
-					{
-						switch (accessor.componentType)
-						{
-						case TINYGLTF_COMPONENT_TYPE_FLOAT:
-						{
-							float *data = (float*)&buffer.data[offset];
-							for (int i = 0; i < accessor.count; i++)
-							{
-								int index = i * 3;
-								FVector Pos = FVector(data[index], data[index + 1], data[index + 2]);
-								Pos = FinalTransform.TransformPosition(Pos);
-								RawTriangles.VertexPositions[VertexOffset + i] = Pos;
-							}
-						}
-						break;
-						case TINYGLTF_COMPONENT_TYPE_DOUBLE:
-						{
-							double *data = (double*)&buffer.data[offset];
-							for (int i = 0; i < accessor.count; i++)
-							{
-								int index = i * 3;
-								FVector Pos = FVector(data[index], data[index + 1], data[index + 2]);
-								Pos = FinalTransform.TransformPosition(Pos);
-								RawTriangles.VertexPositions[VertexOffset + i] = Pos;
-							}
-						}
-						break;
-						default: ensure(false); break;
-						}
-					}
+				case TINYGLTF_COMPONENT_TYPE_FLOAT:  parsePositionData<float>(rawData, accessor->count, RawTriangles, VertexOffset, FinalTransform); break;
+				case TINYGLTF_COMPONENT_TYPE_DOUBLE: parsePositionData<double>(rawData, accessor->count, RawTriangles, VertexOffset, FinalTransform); break;
+				default: ensure(false); break;
 				}
 			}
 		}
 
 
-		const auto &normals = prim.attributes.find("NORMAL");
-		if (normals != prim.attributes.end())
+		rawData = GetAccessorData(model, prim, accessor, "NORMAL");
+		if (rawData && accessor)
 		{
-			int accessorIndex = normals->second;
-			if (accessorIndex >= 0 && accessorIndex < model->accessors.size())
+			RawTriangles.WedgeTangentZ.AddUninitialized(NumFaces * 3);
+			ensure(accessor->type == TINYGLTF_TYPE_VEC3);
+			if (accessor->type == TINYGLTF_TYPE_VEC3)
 			{
-				const tinygltf::Accessor &accessor = model->accessors[accessorIndex];
-				const tinygltf::BufferView &bufferView = model->bufferViews[accessor.bufferView];
-				const tinygltf::Buffer &buffer = model->buffers[bufferView.buffer];
-				size_t offset = accessor.byteOffset + bufferView.byteOffset;
-
+				switch (accessor->componentType)
 				{
-					RawTriangles.WedgeTangentZ.AddUninitialized(NumFaces * 3);
-					ensure(accessor.type == TINYGLTF_TYPE_VEC3);
-					if (accessor.type == TINYGLTF_TYPE_VEC3)
-					{
-						switch (accessor.componentType)
-						{
-						case TINYGLTF_COMPONENT_TYPE_FLOAT:
-						{
-							float *data = (float*)&buffer.data[offset];
-							for (int FaceIdx = 0; FaceIdx < NumFaces; FaceIdx++)
-							{
-								for (int32 CornerIdx = 0; CornerIdx < 3; CornerIdx++)
-								{
-									const int32 WedgeIdx = WedgeOffset + FaceIdx * 3 + CornerIdx;
-									const int32 NormalIdx = TangentZOffset + FaceIdx * 3 + CornerIdx;
-									int32 DataIdx = (RawTriangles.WedgeIndices[WedgeIdx] - VertexOffset) * 3;
-									FVector Normal = FVector(data[DataIdx], data[DataIdx + 1], data[DataIdx + 2]);
-									FVector TransformedNormal = FinalTransformIT.TransformVector(Normal);
-									RawTriangles.WedgeTangentZ[NormalIdx] = TransformedNormal.GetSafeNormal();
-								}
-							}
-						}
-						break;
-						case TINYGLTF_COMPONENT_TYPE_DOUBLE:
-						{
-							double *data = (double*)&buffer.data[offset];
-							for (int FaceIdx = 0; FaceIdx < NumFaces; FaceIdx++)
-							{
-								for (int32 CornerIdx = 0; CornerIdx < 3; CornerIdx++)
-								{
-									const int32 WedgeIdx = WedgeOffset + FaceIdx * 3 + CornerIdx;
-									const int32 NormalIdx = TangentZOffset + FaceIdx * 3 + CornerIdx;
-									int32 DataIdx = (RawTriangles.WedgeIndices[WedgeIdx] - VertexOffset) * 3;
-									FVector Normal = FVector(data[DataIdx], data[DataIdx + 1], data[DataIdx + 2]);
-									FVector TransformedNormal = FinalTransformIT.TransformVector(Normal);
-									RawTriangles.WedgeTangentZ[NormalIdx] = TransformedNormal.GetSafeNormal();
-								}
-							}
-						}
-						break;
-						default: ensure(false); break;
-						}
-					}
+				case TINYGLTF_COMPONENT_TYPE_FLOAT:  parseNormalData<float>(rawData, NumFaces, RawTriangles, VertexOffset, WedgeOffset, TangentZOffset, FinalTransformIT); break;
+				case TINYGLTF_COMPONENT_TYPE_DOUBLE: parseNormalData<double>(rawData, NumFaces, RawTriangles, VertexOffset, WedgeOffset, TangentZOffset, FinalTransformIT); break;
+				default: ensure(false); break;
 				}
 			}
 		}
 
-		const auto &tangent = prim.attributes.find("TANGENT");
-		if (tangent != prim.attributes.end())
+		rawData = GetAccessorData(model, prim, accessor, "TANGENT");
+		if (rawData && accessor)
 		{
-			int accessorIndex = tangent->second;
-			if (accessorIndex >= 0 && accessorIndex < model->accessors.size())
+			RawTriangles.WedgeTangentX.AddUninitialized(NumFaces * 3);
+			switch (accessor->type)
 			{
-				const tinygltf::Accessor &accessor = model->accessors[accessorIndex];
-				const tinygltf::BufferView &bufferView = model->bufferViews[accessor.bufferView];
-				const tinygltf::Buffer &buffer = model->buffers[bufferView.buffer];
-				size_t offset = accessor.byteOffset + bufferView.byteOffset;
-
+			case TINYGLTF_TYPE_VEC3:
+			{
+				switch (accessor->componentType)
 				{
-					RawTriangles.WedgeTangentX.AddUninitialized(NumFaces * 3);
-					switch (accessor.type)
-					{
-					case TINYGLTF_TYPE_VEC3:
-					{
-						switch (accessor.componentType)
-						{
-						case TINYGLTF_COMPONENT_TYPE_FLOAT:
-						{
-							float *data = (float*)&buffer.data[offset];
-							for (int FaceIdx = 0; FaceIdx < NumFaces; FaceIdx++)
-							{
-								for (int32 CornerIdx = 0; CornerIdx < 3; CornerIdx++)
-								{
-									const int32 WedgeIdx = WedgeOffset + FaceIdx * 3 + CornerIdx;
-									int32 DataIdx = (RawTriangles.WedgeIndices[WedgeIdx] - VertexOffset) * 3;
-									FVector Tangent = FVector(data[DataIdx], data[DataIdx + 1], data[DataIdx + 2]);
-									FVector TransformedTangent = FinalTransformIT.TransformVector(Tangent);
-									RawTriangles.WedgeTangentX[WedgeIdx] = TransformedTangent.GetSafeNormal();
-								}
-							}
-						}
-						break;
-						case TINYGLTF_COMPONENT_TYPE_DOUBLE:
-						{
-							double *data = (double*)&buffer.data[offset];
-							for (int32 FaceIdx = 0; FaceIdx < NumFaces; FaceIdx++)
-							{
-								for (int32 CornerIdx = 0; CornerIdx < 3; CornerIdx++)
-								{
-									const int32 WedgeIdx = WedgeOffset + FaceIdx * 3 + CornerIdx;
-									int32 DataIdx = (RawTriangles.WedgeIndices[WedgeIdx] - VertexOffset) * 3;
-									FVector Tangent = FVector(data[DataIdx], data[DataIdx + 1], data[DataIdx + 2]);
-									FVector TransformedTangent = FinalTransformIT.TransformVector(Tangent);
-									RawTriangles.WedgeTangentX[WedgeIdx] = TransformedTangent.GetSafeNormal();
-								}
-							}
-						}
-						break;
-						default: ensure(false); break;
-						}
-					}
-					break;
-					case TINYGLTF_TYPE_VEC4:
-					{
-						switch (accessor.componentType)
-						{
-						case TINYGLTF_COMPONENT_TYPE_FLOAT:
-						{
-							float *data = (float*)&buffer.data[offset];
-							for (int32 FaceIdx = 0; FaceIdx < NumFaces; FaceIdx++)
-							{
-								for (int32 CornerIdx = 0; CornerIdx < 3; CornerIdx++)
-								{
-									const int32 WedgeIdx = WedgeOffset + FaceIdx * 3 + CornerIdx;
-									const int32 TangentXIdx = TangentXOffset + FaceIdx * 3 + CornerIdx;
-									int32 DataIdx = (RawTriangles.WedgeIndices[WedgeIdx] - VertexOffset) * 4;
-									FVector Tangent = FVector(data[DataIdx], data[DataIdx + 1], data[DataIdx + 2]);
-									FVector TransformedTangent = FinalTransformIT.TransformVector(Tangent);
-									RawTriangles.WedgeTangentX[TangentXIdx] = TransformedTangent.GetSafeNormal();
-								}
-							}
-						}
-						break;
-						case TINYGLTF_COMPONENT_TYPE_DOUBLE:
-						{
-							double *data = (double*)&buffer.data[offset];
-							for (int32 FaceIdx = 0; FaceIdx < NumFaces; FaceIdx++)
-							{
-								for (int32 CornerIdx = 0; CornerIdx < 3; CornerIdx++)
-								{
-									const int32 WedgeIdx = WedgeOffset + FaceIdx * 3 + CornerIdx;
-									const int32 TangentXIdx = TangentXOffset + FaceIdx * 3 + CornerIdx;
-									int32 DataIdx = (RawTriangles.WedgeIndices[WedgeIdx] - VertexOffset) * 4;
-									FVector Tangent = FVector(data[DataIdx], data[DataIdx + 1], data[DataIdx + 2]);
-									FVector TransformedTangent = FinalTransformIT.TransformVector(Tangent);
-									RawTriangles.WedgeTangentX[TangentXIdx] = TransformedTangent.GetSafeNormal();
-								}
-							}
-						}
-						break;
-						default: ensure(false); break;
-						}
-					}
-					break;
-					default: ensure(false); break;
-					}
+				case TINYGLTF_COMPONENT_TYPE_FLOAT:  parseTangentData<float>( rawData, NumFaces, RawTriangles, VertexOffset, WedgeOffset, TangentXOffset, true, FinalTransformIT); break;
+				case TINYGLTF_COMPONENT_TYPE_DOUBLE: parseTangentData<double>(rawData, NumFaces, RawTriangles, VertexOffset, WedgeOffset, TangentXOffset, true, FinalTransformIT); break;
+				default: ensure(false); break;
 				}
+			}
+			break;
+			case TINYGLTF_TYPE_VEC4:
+			{
+				switch (accessor->componentType)
+				{
+				case TINYGLTF_COMPONENT_TYPE_FLOAT:  parseTangentData<float>( rawData, NumFaces, RawTriangles, VertexOffset, WedgeOffset, TangentXOffset, false, FinalTransformIT); break;
+				case TINYGLTF_COMPONENT_TYPE_DOUBLE: parseTangentData<double>(rawData, NumFaces, RawTriangles, VertexOffset, WedgeOffset, TangentXOffset, false, FinalTransformIT); break;
+				default: ensure(false); break;
+				}
+			}
+			break;
+			default: ensure(false); break;
 			}
 		}
 
@@ -593,113 +413,19 @@ UStaticMesh* FGLTFStaticMeshImporter::ImportStaticMesh(FGLTFImportContext& Impor
 bool FGLTFStaticMeshImporter::AddUVs(const tinygltf::Model *model, const tinygltf::Primitive &prim, FRawMesh &RawTriangles, int32 NumFaces, int32 WedgeOffset, int32 VertexOffset, int32 uvIndex)
 {
 	bool uvsAdded = false;
-	std::string name = "TEXCOORD_" + std::to_string(uvIndex);
-	const auto &texcoords = prim.attributes.find(name);
-	if (texcoords != prim.attributes.end())
+
+	tinygltf::Accessor* accessor = nullptr;
+	unsigned char* rawData;
+
+	rawData = GetAccessorData(model, prim, accessor, "TEXCOORD_" + std::to_string(uvIndex));
+	if (rawData && accessor)
 	{
-		int accessorIndex = texcoords->second;
-		if (accessorIndex >= 0 && accessorIndex < model->accessors.size())
+		ensure(accessor->type == TINYGLTF_TYPE_VEC2);
+		switch (accessor->componentType)
 		{
-			const tinygltf::Accessor &accessor = model->accessors[accessorIndex];
-			const tinygltf::BufferView &bufferView = model->bufferViews[accessor.bufferView];
-			const tinygltf::Buffer &buffer = model->buffers[bufferView.buffer];
-			size_t offset = accessor.byteOffset + bufferView.byteOffset;
-
-			if (accessor.count > 0)
-			{
-				//One uv per vertex on every polygon
-				RawTriangles.WedgeTexCoords[uvIndex].AddUninitialized(NumFaces * 3);
-				TArray<FVector2D>& TexCoords = RawTriangles.WedgeTexCoords[uvIndex];
-				ensure(accessor.type == TINYGLTF_TYPE_VEC2);
-				if (accessor.type == TINYGLTF_TYPE_VEC2)
-				{
-					switch (accessor.componentType)
-					{
-					case TINYGLTF_COMPONENT_TYPE_FLOAT:
-					{
-						int32 WedgesCount = RawTriangles.WedgeIndices.Num();
-						int32 TexCoordsCount = TexCoords.Num();
-						float *data = (float*)&buffer.data[offset];
-						for (int FaceIdx = 0; FaceIdx < NumFaces; FaceIdx++)
-						{
-							const int32 I0 = WedgeOffset + FaceIdx * 3 + 0;
-							const int32 I1 = WedgeOffset + FaceIdx * 3 + 1;
-							const int32 I2 = WedgeOffset + FaceIdx * 3 + 2;
-
-							if (I0 >= WedgesCount || I1 >= WedgesCount || I2 >= WedgesCount)
-							{
-								ensure(false);
-								break;
-							}
-
-							if (I0 >= TexCoordsCount || I1 >= TexCoordsCount || I2 >= TexCoordsCount)
-							{
-								ensure(false);
-								break;
-							}
-
-							int32 index0 = RawTriangles.WedgeIndices[I0] - VertexOffset;
-							int32 index1 = RawTriangles.WedgeIndices[I1] - VertexOffset;
-							int32 index2 = RawTriangles.WedgeIndices[I2] - VertexOffset;
-
-							if (index0 >= accessor.count || index1 >= accessor.count || index2 >= accessor.count)
-								break;
-
-							index0 *= 2;
-							index1 *= 2;
-							index2 *= 2;
-
-							TexCoords[I0] = FVector2D(data[index0], data[index0 + 1]);
-							TexCoords[I1] = FVector2D(data[index1], data[index1 + 1]);
-							TexCoords[I2] = FVector2D(data[index2], data[index2 + 1]);
-						}
-						uvsAdded = true;
-					}
-					break;
-					case TINYGLTF_COMPONENT_TYPE_DOUBLE:
-					{
-						int32 WedgesCount = RawTriangles.WedgeIndices.Num();
-						int32 TexCoordsCount = TexCoords.Num();
-						double *data = (double*)&buffer.data[offset];
-						for (int FaceIdx = 0; FaceIdx < NumFaces; FaceIdx++)
-						{
-							const int32 I0 = WedgeOffset + FaceIdx * 3 + 0;
-							const int32 I1 = WedgeOffset + FaceIdx * 3 + 1;
-							const int32 I2 = WedgeOffset + FaceIdx * 3 + 2;
-
-							if (I0 >= WedgesCount || I1 >= WedgesCount || I2 >= WedgesCount)
-							{
-								ensure(false);
-								break;
-							}
-
-							if (I0 >= TexCoordsCount || I1 >= TexCoordsCount || I2 >= TexCoordsCount)
-							{
-								ensure(false);
-								break;
-							}
-
-							int32 index0 = RawTriangles.WedgeIndices[I0] - VertexOffset;
-							int32 index1 = RawTriangles.WedgeIndices[I1] - VertexOffset;
-							int32 index2 = RawTriangles.WedgeIndices[I2] - VertexOffset;
-
-							if (index0 >= accessor.count || index1 >= accessor.count || index2 >= accessor.count)
-								break;
-
-							index0 *= 2;
-							index1 *= 2;
-							index2 *= 2;
-
-							TexCoords[I0] = FVector2D(data[index0], data[index0 + 1]);
-							TexCoords[I1] = FVector2D(data[index1], data[index1 + 1]);
-							TexCoords[I2] = FVector2D(data[index2], data[index2 + 1]);
-						}
-					}
-					break;
-					default: ensure(false); break;
-					}
-				}
-			}
+		case TINYGLTF_COMPONENT_TYPE_FLOAT:  parseUVs<float>(RawTriangles, rawData, accessor, NumFaces, WedgeOffset, VertexOffset, uvIndex); uvsAdded = true;  break;
+		case TINYGLTF_COMPONENT_TYPE_DOUBLE: parseUVs<double>(RawTriangles, rawData, accessor, NumFaces, WedgeOffset, VertexOffset, uvIndex); uvsAdded = true; break;
+		default: ensure(false); break;
 		}
 	}
 
